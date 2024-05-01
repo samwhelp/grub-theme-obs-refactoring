703790a8b961b57f638bc8099c89d318295d641f
samwhelp <samwhelp@users.noreply.github.com>
2024-05-01 20:58:07 +0800

    fix

---
+++ source/grub-theme/grub-theme-obs/README.md
@@ -0,0 +1,10 @@
+
+
+# Grub Theme / Source / grub-theme-obs
+
+
+## Link
+
+| Link |
+| ---- |
+| [GitHub](https://github.com/obster-y/grub-theme-obs) |
---
+++ source/grub-theme/grub-theme-obs/clone.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+##
+## ## Link
+##
+## * [GitHub](https://github.com/obster-y/grub-theme-obs)
+##
+
+
+git clone https://github.com/obster-y/grub-theme-obs.git grub-theme-obs
+
+
+rm -rf grub-theme-obs/.git
